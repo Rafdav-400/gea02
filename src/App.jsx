151e9579879cb46021559fc88f7f93f75d028897
Rafdav-400 <rafael.velez.2211@gmail.com>
2023-12-11 21:33:46 -0600

    Solo falta arreglar el local storage

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Header from './components/Header';
 import Formulario from './components/Formulario';
 import ListadoPacientes from './components/ListadoPacientes';
@@ -6,6 +6,19 @@ import ListadoPacientes from './components/ListadoPacientes';
 const App = () => {
   const [paciente, setPaciente] = useState([])
   const [cliente, setCliente] = useState({})
+
+  useEffect(() => {
+    const obtenerLS = () => {
+      const pacientesLS = JSON.parse(localStorage.getItem("paciente")) ?? []
+      setPaciente(pacientesLS)
+    }
+    obtenerLS()
+  }, [])
+
+  useEffect(() => {
+    {/*console.log("Componente listo o cambió pacientes")*/}
+    localStorage.setItem("paciente",JSON.stringify(paciente))
+  }, [paciente])
 
   const eliminarPaciente = id => {
     const pacienteActualizado = paciente.filter(cliente => cliente.id !== id)
